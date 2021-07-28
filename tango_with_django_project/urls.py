81f0390265907d55c8b841ade5c0a74ed6b6a5f2
Jose Mendoza <sagaka1g@gmail.com>
2021-07-29 00:53:02 +0100

    Chapter 8 updated

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,7 +23,5 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('about/', views.about, name='about'),
-    # The above maps any URLs starting with rango/ to be handled by rango
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
